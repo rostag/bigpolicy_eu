ede950253e71d2b4ec8b701cd214dfdc6ac60c80
Serhii <hello@gwaramedia.com.ua>
2016-06-08 22:42:52 +0300

    fixed div and added grid basics

--- src/client/app/leader/edit/leader.edit.component.ts
+++ src/client/app/leader/edit/leader.edit.component.ts
@@ -5,15 +5,18 @@ import {MD_INPUT_DIRECTIVES} from '@angular2-material/input/input';
 import {MdButton} from '@angular2-material/button/button';
 import {MdCard} from '@angular2-material/card/card';
 import {MdCheckbox} from '@angular2-material/checkbox/checkbox';
-import {MdIcon} from '@angular2-material/icon/icon';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon/icon';
 import {MdToolbar} from '@angular2-material/toolbar/toolbar';
+import {MD_GRID_LIST_DIRECTIVES} from '@angular2-material/grid-list/grid-list';
+
 
 import {NameListService} from '../../shared/index';
 @Component({
   templateUrl: 'app/leader/edit/leader.edit.component.html',
   styleUrls: ['app/leader/edit/leader.edit.component.css'],
-  directives: [FORM_DIRECTIVES, MdCard, MdCheckbox, MdButton, MdIcon, MdToolbar, MD_INPUT_DIRECTIVES]
-})
+  directives: [FORM_DIRECTIVES, MdCard, MdCheckbox, MdButton, MdIcon, MdToolbar, MD_INPUT_DIRECTIVES, MD_GRID_LIST_DIRECTIVES]
+  providers: [MdIconRegistry]
+  })
 
 export class EditLeaderComponent {
   newName: string;
