86f16ad5d1a3837a5f9d8cb17de6cbcf0233d56d
Rost Siryk <rostislav.siryk@gmail.com>
2016-06-18 16:30:02 +0100

    leader view added; cleanup

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,18 @@ import { AboutComponent } from './app/about/index';
 import { LandingComponent } from './app/landing/index';
 import { EditLeaderComponent } from './app/leader/edit/index';
 import { LeadersListComponent } from './app/leader/list/index';
+import { LeaderViewComponent } from './app/leader/view/index';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
-  HTTP_PROVIDERS
-  ,
+  HTTP_PROVIDERS,
   provideRouter([
     { index: true, component: LandingComponent },
     { path: '/about', component: AboutComponent },
+    { path: '/leader', component: LeaderViewComponent },
     { path: '/leaders', component: LeadersListComponent },
     { path: '/add-leader', component: EditLeaderComponent }
   ])
