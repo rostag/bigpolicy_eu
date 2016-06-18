48c8cc0e03a7e3b26bf056c5fcf83f9f5de90b65
Rost Siryk <rostislav.siryk@gmail.com>
2016-06-18 07:46:28 +0100

    bp-local-cli-open-express

---
+++ .openshift/express.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.use(express.static('./'));
+
+app.listen(server_port, server_ip_address, function () {
+  console.log('Big Policy is listening on port ' + server_port);
+});
--- start.js
+++ start.js
@@ -1,40 +1,11 @@
-const cluster = require('cluster'),
-      stopSignals = [
-        'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-        'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-      ],
-      production = process.env.NODE_ENV == 'production';
+var express = require('express');
+var app = express();
 
-let stopping = false;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-cluster.on('disconnect', function(worker) {
-  if (production) {
-    if (!stopping) {
-      cluster.fork();
-    }
-  } else {
-    process.exit(1);
-  }
+app.use(express.static('./dist'));
+
+app.listen(server_port, server_ip_address, function () {
+  console.log('Big Policy is listening on port ' + server_port);
 });
-
-if (cluster.isMaster) {
-  const workerCount = process.env.NODE_CLUSTER_WORKERS || 4;
-  console.log(`Starting ${workerCount} workers...`);
-  for (let i = 0; i < workerCount; i++) {
-    cluster.fork();
-  }
-  if (production) {
-    stopSignals.forEach(function (signal) {
-      process.on(signal, function () {
-        console.log(`Got ${signal}, stopping workers...`);
-        stopping = true;
-        cluster.disconnect(function () {
-          console.log('All workers stopped, exiting.');
-          process.exit(0);
-        });
-      });
-    });
-  }
-} else {
-  require('./app.js');
-}
