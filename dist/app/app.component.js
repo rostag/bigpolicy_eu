9975142cc9acdda9ec7c3707ee32fed1b2587d22
Rost Siryk <rostislav.siryk@gmail.com>
2016-06-19 15:26:55 +0100

    splash animated

---
+++ dist/app/app.component.js
@@ -0,0 +1,37 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var card_1 = require('@angular2-material/card');
+var button_1 = require('@angular2-material/button');
+var icon_1 = require('@angular2-material/icon');
+var http_1 = require('@angular/http');
+var router_1 = require('@angular/router');
+var index_1 = require('./shared/index');
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'BigPolicy';
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'app-root',
+            templateUrl: 'app.component.html',
+            styleUrls: ['app.component.css'],
+            viewProviders: [http_1.HTTP_PROVIDERS],
+            directives: [router_1.ROUTER_DIRECTIVES, card_1.MD_CARD_DIRECTIVES, button_1.MD_BUTTON_DIRECTIVES, icon_1.MdIcon, index_1.NavbarComponent, index_1.ToolbarComponent],
+            providers: [icon_1.MdIconRegistry]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
