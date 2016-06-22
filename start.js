63289b6124551dc895095c5a87ee5ac5620c66d3
Rost Siryk <rostislav.siryk@gmail.com>
2016-06-22 10:38:38 +0100

    deploy-update

--- start.js
+++ start.js
@@ -1,11 +1,27 @@
+var http = require('http');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var hostname = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.2';
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+function redirectToSecure(req, res, next) {
+  if (req.headers['x-forwarded-proto'] == 'http' || req.headers['x-forwarded-proto'] == null) {
+      res.redirect('https://' + req.headers.host + req.path);
+      return;
+  } else {
+    next();
+  }
+}
 
-app.use(express.static('./dist'));
+if (hostname === '127.0.0.2') {
+  port = 80;
+} else {
+  app.use(redirectToSecure);
+}
 
-app.listen(server_port, server_ip_address, function () {
-  console.log('Big Policy is listening on port ' + server_port);
-});
+app.use(express.static(__dirname + '/dist'));
+
+server.listen(port,hostname);
+
+console.log( 'BigPolicy Express web server listening on port ' + port + ' , host: ' + hostname);
