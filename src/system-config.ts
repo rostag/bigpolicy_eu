b80019c7fd6f4beff69d8f20d7159ec7078c33dc
Prokop <prokopenko.serhii@gmail.com>
2016-07-22 21:10:31 +0300

    Added package to fix errors in landing after splash screen

--- src/system-config.ts
+++ src/system-config.ts
@@ -47,6 +47,7 @@ const barrels: string[] = [
   '@angular/common',
   '@angular/compiler',
   '@angular/http',
+  '@angular/forms',
   '@angular/router',
   '@angular/platform-browser',
   '@angular/platform-browser-dynamic',
