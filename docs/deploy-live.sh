5fc90a7bf51cb2a5f640c4594042f87782915877
Rostyslav Siryk <rostislav.siryk@gmail.com>
2016-09-21 01:32:54 +0300

    editor forms fixed
    pre-deploy update

--- docs/deploy-live.sh
+++ docs/deploy-live.sh
@@ -5,16 +5,13 @@
 git checkout -B deploy-live-bck-$1 deploy-live
 
 git checkout deploy-live
-git merge $2 -Xtheirs -m 'deploy-update'
+git merge $2 -X theirs -m 'deploy-update'
 
 ng build -prod
-
-#Workaround to copy auth0 assets
-cp -r src/assets/js/ dist/assets/js
 
 git add .
 git commit -m 'deploy-update-build'
 
 git push deploy-live deploy-live
 
-git checkout $2
+git checkout -B feat-temp
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,33 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+// FIXME import { LocalStorageService, LOCAL_STORAGE_SERVICE_CONFIG } from 'angular-2-local-storage';
+
+/**
+ * Application bootstrap, or entry point where it all starts
+ */
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// WAS: (FIXME: CLEANUP)
+// Create config options (see ILocalStorageServiceConfigOptions) for deets:
+// let localStorageServiceConfig = {
+//     prefix: 'bp-app',
+//     storageType: 'sessionStorage'
+// };
+
+// Provide the config to the service:
+// const LOCAL_STORAGE_CONFIG_PROVIDER: Provider = provide(LOCAL_STORAGE_SERVICE_CONFIG, {
+//     useValue: localStorageServiceConfig
+// });
+
+// bootstrap(AppComponent, [
+//   ...
+//   // FIXME
+//   LOCAL_STORAGE_CONFIG_PROVIDER,
+//   APP_ROUTES_PROVIDER
+// ]);
