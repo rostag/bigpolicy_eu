c2fc8c53686d7580afaf1741c91fdda58c9aedf8
Rostyslav Siryk <rostislav.siryk@gmail.com>
2016-10-29 17:28:01 +0100

    video-control-update-value-fix

--- src/app/shared/video/video.component.ts
+++ src/app/shared/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -6,36 +6,38 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
-export class VideoComponent implements OnInit {
 
-  @Input() videoUrl: string = '';
+export class VideoComponent {
+
+  // Unused at the moment, but will be needed for images alt text
   @Input() title: string = '';
 
-  constructor( private sanitizer: DomSanitizer ) { }
+  // Main input, a media (video) url
+  @Input()
+  set videoUrl(url: string) {
+    // Set YouTube ID:
+    if (url) {
+      // FIXME it's being called too many times
+      var match = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
+      this.youTubeId = (match && match[7].length == 11) ? match[7] : null;
 
-  ngOnInit() { }
+      // Set safe media URL, TODO: BP_SECURITY
+      this.safeMediaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        this.youTubeId
+          ? 'https://www.youtube.com/embed/' + this.youTubeId
+          : null
+      );
 
-  // TODO: BP_SECURITY
-  get safeVideoUrl() {
-    var videoUrl = this.youTubeId
-      ? 'https://www.youtube.com/embed/' + this.youTubeId
-      : null;
-    return this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl);
-  }
-
-  get youTubeId() {
-    if (!this.videoUrl) {
-      return null;
+      // set thumb URL
+      this.thumbUrl = this.youTubeId
+          ? 'http://img.youtube.com/vi/' + this.youTubeId + '/0.jpg'
+          : 'assets/img/project/project-placeholder.png';
     }
-    // FIXME it's being called too many times
-    var match = this.videoUrl.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
-    return (match && match[7].length == 11) ? match[7] : null;
-  }
+  };
 
-  get videoThumbUrl() {
-    return this.youTubeId
-      ? 'http://img.youtube.com/vi/' + this.youTubeId + '/0.jpg'
-      : 'assets/img/project/project-placeholder.png';
-  }
+  private safeMediaUrl;
+  private thumbUrl;
+  private youTubeId;
 
+  constructor( private sanitizer: DomSanitizer ) { }
 }
