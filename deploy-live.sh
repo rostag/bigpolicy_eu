ec37f5d5be9f7e039ccd2329697b4a17067dd222
Rostyslav Siryk <rostislav.siryk@gmail.com>
2016-10-30 01:30:03 +0000

    deploy-script-update

--- deploy-live.sh
+++ deploy-live.sh
@@ -6,6 +6,7 @@ git checkout -B deploy-live-bck-$1 deploy-live
 
 git checkout deploy-live
 git merge $2 -X theirs -m 'deploy-update'
+git merge 0221f2bd785d4961a1decea518fe5a4dd47b6f75 -X theirs -m 'gitignore-restore'
 
 ng build -prod
 
