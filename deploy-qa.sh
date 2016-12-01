948683803bb26441e8a033ae31a00aa49ca211ee
Rostyslav Siryk <rostislav.siryk@gmail.com>
2016-12-01 17:18:21 +0000

    post-demo

--- deploy-qa.sh
+++ deploy-qa.sh
@@ -6,12 +6,10 @@ git checkout -B deploy-qa-bck-$1 deploy-qa
 
 git checkout deploy-qa
 git merge $2 -X theirs -m 'deploy-update'
-git merge 0221f2bd785d4961a1decea518fe5a4dd47b6f75 -X theirs -m 'gitignore-restore'
 
 ng build -prod
 
-git add .
-git commit -m 'deploy-update-build'
+git add . && git commit -m 'deploy-update-build'
 
 git push deploy-qa deploy-qa --force
 
