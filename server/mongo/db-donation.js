ef55ee5e6a7e6ca7907701b6623c1168a3b07e50
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-02-01 22:36:46 +0000

    fix-donation-external-id

--- server/mongo/db-donation.js
+++ server/mongo/db-donation.js
@@ -43,9 +43,6 @@ DBDonation.createDonation = function(data) {
 
   try {
     var model = new Donation(data);
-    // we use mongo ID here, to use it later as back reference for order_id in liqpay order status callback
-    model.externalId = 'bpdon___id_' + model._id + '__amt_' + model.amount + '__from_' + model.donorId + '__to_' + model.targetId + '__type_' + model.targetType + '__t_' + Date.now();
-    console.log('DBDonation.createDonation. externalId = ', model.externalId);
   } catch (error){
     throw ( 'DBDonation: Invalid donation cannot be saved.')
   }
@@ -57,10 +54,12 @@ DBDonation.updateDonation = function(id, data) {
   console.log('DBDonation: updateDonation', id, data)
 
   return Donation.findById(id, function(err, model) {
+    console.log(' -> updateDonation', err, model)
     if (err || !model || !data) {
       return;
     }
     for (var field in data) {
+      console.log(' -> field:', field, model[field])
       model[field] = data[field]
     }
     return model.save();
