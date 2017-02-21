3e3aa0b3640eaa37886d5b73d1a5a60a66a1c8c8
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-02-21 03:29:36 +0200

    gdrive-upload-is-working-cleanup

--- src/app/shared/drive/realtime/realtime.component.ts
+++ src/app/shared/drive/realtime/realtime.component.ts
@@ -2,9 +2,7 @@
 /// <reference path="../../../../../node_modules/@types/gapi.auth2/index.d.ts"/>
 /// <reference path="../../../../../node_modules/@types/google-drive-realtime-api/index.d.ts"/>
 /// <reference path="./google-drive-api.d.ts"/>
-
 import { Component, OnInit, AfterViewInit, Input, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-
 import { Http, RequestOptions, Headers, URLSearchParams} from '@angular/http';
 
 @Component({
@@ -26,34 +24,16 @@ export class RealtimeComponent implements AfterViewInit {
 
   files = [];
 
-  DEFAULT_FILE = {
-    content: 'hi from bp!',
-    metadata: {
-      id: null,
-      title: 'untitled.txt',
-      mimeType: 'text/plain',
-      editable: true
-    }
-  };
-
-  file = this.DEFAULT_FILE;
-
-  urlToUpload = '';
-
   constructor(
     private ref: ChangeDetectorRef
   ) {}
 
   /**
-   *  On load, called to load the auth2 library and API client library.
-   */
-  handleClientLoad() {
-    console.log('gapi:', gapi);
-    gapi.load('client:auth2', () => { this.initClient(this); });
-  }
-
+  *  On load, called to load the auth2 library and API client library.
+  */
   ngAfterViewInit() {
-    this.handleClientLoad();
+    // console.log('gapi:', gapi);
+    gapi.load('client:auth2', () => { this.initClient(this); });
   }
 
   /**
@@ -74,9 +54,6 @@ export class RealtimeComponent implements AfterViewInit {
       'https://www.googleapis.com/auth/drive.appdata',
       'https://www.googleapis.com/auth/drive.file'
     ].join(' ');
-
-    console.log('this is ', this);
-    // console.log('that is ', that);
 
     gapi.client.init({
       discoveryDocs: DISCOVERY_DOCS,
@@ -124,68 +101,8 @@ export class RealtimeComponent implements AfterViewInit {
   }
 
   handleAddFileClick(event) {
-    // this.createFile();
-
     this.upload();
-    // this.uploadFile3(this.fileToUpload);
-    // this.uploadFileMultipart();
-    // this.uploadFile();
-    // this.createFileWithJSONContent('BP_1', {'hi': 'hi'}, res => console.log(res));
-    // this.preupload(
-    //   'https://www.googleapis.com/upload/drive/v3/files?uploadType=media',
-    //   this.savedSignInUserInfo.getAuthResponse().access_token
-    // );
-
     return false;
-  }
-
-  createFile() {
-    // this.save();
-    gapi.client.drive.files.create({
-      uploadType: 'media',
-      ignoreDefaultVisibility: true,
-      useContentAsIndexableText: true,
-
-    }).then( (response) => {
-      console.log('response:', response);
-    });
-  }
-
-  createFileWithJSONContent (name, data, callback) {
-    const boundary = '-------314159265358979323846';
-    const delimiter = '\r\n--' + boundary + '\r\n';
-    const close_delim = '\r\n--' + boundary + '--';
-
-    const contentType = 'application/json';
-
-    const metadata = {
-        'name': name,
-        'mimeType': contentType
-      };
-
-    const multipartRequestBody =
-          delimiter +
-          'Content-Type: application/json\r\n\r\n' +
-          JSON.stringify(metadata) +
-          delimiter +
-          'Content-Type: ' + contentType + '\r\n\r\n' +
-          data +
-          close_delim;
-
-    const request = gapi.client.request({
-          'path': '/upload/drive/v3/files',
-          'method': 'POST',
-          'params': {'uploadType': 'multipart'},
-          'headers': {
-            'Content-Type': 'multipart/related; boundary="' + boundary + '"'
-          },
-          'body': multipartRequestBody});
-      if (!callback) {
-        callback = function(file) {
-          console.log(file);
-        };
-      }
-      request.execute(callback);
   }
 
   handleUploadFilenameChange(evt, a, b) {
@@ -198,236 +115,55 @@ export class RealtimeComponent implements AfterViewInit {
       }
       this.fileToUpload = evt.target.files[0];
       this.fileToUploadName = filename;
-      console.log('filename:', filename, this.fileToUpload);
     }
-  }
-
-  uploadFile3(text, callback = null) {
-
-      const boundary = '287032381131322';
-      const delimiter = '\r\n--' + boundary + '\r\n';
-      const close_delim = '\r\n--' + boundary + '--';
-
-      const contentType = this.fileToUpload.type || 'multipart/form-data';
-      const metadata = {
-        'mimeType': contentType,
-        'name': this.fileToUploadName,
-        'description': 'Oh my file =)'
-      };
-
-      const reader = new FileReader();
-
-      reader.onload = function(evt: any) {
-        // xhr.send(evt.target.result);
-
-        console.log('reader', contentType, evt);
-
-        const multipartRequestBody =
-            delimiter +  'Content-Type: application/json\r\n\r\n' +
-            JSON.stringify(metadata) +
-            delimiter + 'Content-Type: ' + contentType + '\r\n' + '\r\n' +
-            evt.target.result +
-            close_delim;
-
-        if (!callback) {
-          callback = function(file) { console.log('Update Complete ', file); };
-        }
-
-        gapi.client.request({
-          'path': 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart',
-          'method': 'POST',
-          'params': {'uploadType': 'multipart'},
-          'headers': {'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'},
-          'body': multipartRequestBody,
-          callback: callback
-        });
-      };
-      reader.readAsBinaryString(text);
-    }
-
-  // WIP
-  uploadFileMultipart() {
-    const form = new FormData();
-    const xhttp = new XMLHttpRequest();
-    xhttp.responseType = 'blob';
-
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-        console.log('Uploaded');
-      } else if (xhttp.readyState === 4 ) {
-        console.log('Upload result:', xhttp.status, xhttp.response);
-      }
-    };
-
-    const formData: any = new FormData();
-    formData.append('myFile', this.fileToUpload);
-    // formData.append('title', 'file_name.extension');
-    // formData.append('mimeType', 'mime/type');
-    // formData.append('description', 'Stuff about the file');
-
-    xhttp.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', true);
-    xhttp.setRequestHeader('Authorization', 'Bearer ' + this.savedSignInUserInfo.getAuthResponse().access_token);
-    xhttp.send(this.fileToUpload);
-    // xhttp.send(formData);
-  }
-
-  uploadFile() {
-    const form = new FormData();
-    const xhttp = new XMLHttpRequest();
-    xhttp.responseType = 'blob';
-
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-        console.log('Uploaded');
-      } else if (xhttp.readyState === 4 ) {
-        console.log('Upload result:', xhttp.status, xhttp.response);
-      }
-
-    };
-    xhttp.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media', true);
-    xhttp.setRequestHeader('Authorization', 'Bearer ' + this.savedSignInUserInfo.getAuthResponse().access_token);
-    xhttp.send(this.fileToUpload);
   }
 
   /**
    * Initiate the upload.
    */
-  preupload(url, token) {
+  upload() {
     const self = this;
     const xhr = new XMLHttpRequest();
-    const fileMetadata = {
-      name: this.fileToUploadName,
-      title: this.fileToUploadName
-    };
-
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    // xhr.setRequestHeader('X-Upload-Content-Length', size);
-    // xhr.setRequestHeader('X-Upload-Content-Type', contentType);
-
-    xhr.onload = function(e) {
-      const location = e.target.getResponseHeader('Location');
-      this.url = location;
-      this.uploadFile();
-    }.bind(this);
-    // xhr.onerror = this.onUploadError_.bind(this);
-    xhr.send(JSON.stringify(fileMetadata));
-  };
-
-  // WIP WIP WIP
-  uploadDocFile(name, data, callback) {
-    const boundary = '-------314159265358979323846';
-    const delimiter = '\r\n--' + boundary + '\r\n';
-    const close_delim = '\r\n--' + boundary + '--';
-
-    const contentType = 'application/msword';
 
     const metadata = {
-      'name': name,
-      'mimeType': contentType
+      'name': this.fileToUpload.name,
+      'title': this.fileToUpload.name,
+      'mimeType': this.fileToUpload.type,
+      'description': 'Stuff about the file'
     };
 
-    const multipartRequestBody =
-          delimiter +
-          'Content-Type: ' + contentType + '\r\n\r\n' +
-          JSON.stringify(metadata) +
-          delimiter +
-          'Content-Type: ' + contentType + '\r\n\r\n' +
-          data +
-          close_delim;
+    xhr.open('POST', 'https://www.googleapis.com/drive/v3/files?uploadType=multipart', true);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + this.savedSignInUserInfo.getAuthResponse().access_token);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Upload-Content-Length', this.fileToUpload.size);
+    xhr.setRequestHeader('X-Upload-Content-Type', this.fileToUpload.type);
 
-    const request = gapi.client.request({
-          'path': '/upload/drive/v3/files',
-          'method': 'POST',
-          'params': {'uploadType': 'multipart'},
-          'headers': {
-            'Content-Type': 'multipart/related; boundary="' + boundary + '"'
-          },
-          'body': multipartRequestBody});
-      if (!callback) {
-        callback = function(file) {
-          console.log(file);
-        };
-      }
-      request.execute(callback);
-  }
+    xhr.onload = (e: any) => {
+      const resp = JSON.parse(e.target.response);
+      this.sendFile(resp.id);
+    };
+    xhr.onerror = (err) => console.log('upload error:', err);
+    xhr.send(JSON.stringify(metadata));
+  };
 
   /**
-     * Internal helper to saves the current file. If the file is new, redirects to the correct URL once complete.
-     *
-     * @return {Promise} promise that resolves once the save is complete
-     */
-    save() {
-      console.log('File to be saved');
-      gapi.client.drive.files.save(this.file.metadata, this.file.content).then((result) => {
-        this.redirectIfChanged(result.metadata.id);
-        this.file = result;
-        console.log('File saved');
-        return this.file;
-      }, (err) => {
-        console.log('Unable to save file');
-      });
-    };
+   * Send the actual file content.
+   *
+   * @private
+   */
+  sendFile(fileId) {
+    const content = this.fileToUpload;
+    const end = this.fileToUpload.size;
 
-    redirectIfChanged (id) {
-      // if (this.file.metadata.id != id) {
-      //   location.path('/edit/' + id);
-      //   location.search('');
-      //   location.replace();
-      // }
-    };
-
-
-    /**
-     * Initiate the upload.
-     */
-    upload() {
-      const self = this;
-      const xhr = new XMLHttpRequest();
-
-      const metadata = {
-        'name': this.fileToUpload.name,
-        'title': this.fileToUpload.name,
-        'mimeType': this.fileToUpload.type,
-        'description': 'Stuff about the file'
-      };
-
-      xhr.open('POST', 'https://www.googleapis.com/drive/v3/files?uploadType=multipart', true);
-      xhr.setRequestHeader('Authorization', 'Bearer ' + this.savedSignInUserInfo.getAuthResponse().access_token);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('X-Upload-Content-Length', this.fileToUpload.size);
-      xhr.setRequestHeader('X-Upload-Content-Type', this.fileToUpload.type);
-
-      xhr.onload = (e: any) => {
-        const resp = JSON.parse(e.target.response);
-        console.log('upload e:', e, resp);
-        this.sendFile(resp.id);
-      };
-      xhr.onerror = (err) => console.log('upload error:', err);
-      xhr.send(JSON.stringify(metadata));
-    };
-
-    /**
-     * Send the actual file content.
-     *
-     * @private
-     */
-    sendFile(fileId) {
-      const content = this.fileToUpload;
-      const end = this.fileToUpload.size;
-
-      const xhr = new XMLHttpRequest();
-      xhr.open('PATCH', 'https://www.googleapis.com/upload/drive/v3/files/' + fileId, true);
-      xhr.setRequestHeader('Content-Type', this.fileToUpload.type);
-      xhr.setRequestHeader('Authorization', 'Bearer ' + this.savedSignInUserInfo.getAuthResponse().access_token);
-      // xhr.setRequestHeader('Content-Range', 'bytes ' + 0 + '-' + (this.fileToUpload.size - 1) + '/' + this.fileToUpload.size);
-      xhr.setRequestHeader('X-Upload-Content-Type', this.fileToUpload.type);
-      // xhr.setRequestHeader('X-Upload-Content-Length', this.fileToUpload.size);
-      xhr.onload = (res) => console.log('upload res:', res);
-      xhr.onerror = (err) => console.log('upload error:', err);
-      xhr.send(content);
-    };
+    const xhr = new XMLHttpRequest();
+    xhr.open('PATCH', 'https://www.googleapis.com/upload/drive/v3/files/' + fileId, true);
+    xhr.setRequestHeader('Content-Type', this.fileToUpload.type);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + this.savedSignInUserInfo.getAuthResponse().access_token);
+    xhr.setRequestHeader('X-Upload-Content-Type', this.fileToUpload.type);
+    xhr.onload = (res) => console.log('upload res:', res);
+    xhr.onerror = (err) => console.log('upload error:', err);
+    xhr.send(content);
+  };
 
   /**
    * Print files.
@@ -452,7 +188,4 @@ export class RealtimeComponent implements AfterViewInit {
       }
     });
   }
-
-  ////////////////////////////////////////////////////////////////////////////
-
 }
