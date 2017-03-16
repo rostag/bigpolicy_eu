0b70316d9cbb8d285948ea74d7c69c017009522f
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-03-16 00:36:25 +0000

    user-can-edit-his-own-files-only

--- src/app/shared/files/view/files.view.component.ts
+++ src/app/shared/files/view/files.view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-files-view',
   templateUrl: './files.view.component.html',
-  styleUrls: ['./files.view.component.css']
+  styleUrls: ['./files.view.component.scss']
 })
 export class FilesViewComponent implements OnInit {
 
