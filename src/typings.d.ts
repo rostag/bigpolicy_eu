65faf340f6227208322703adc024d0a64d035054
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-03-22 00:51:18 +0000

    Version and build number autoincrement and injection into angular2 app

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,10 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
+
+// Used for app version injection via json
+// Details: https://hackernoon.com/import-json-into-typescript-8d465beded79
+declare module '*.json' {
+    const value: any;
+    export default value;
+}
