7294ef09b5f5792a521e8be72277fc1dacf3a119
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-03-28 08:56:43 +0100

    WIP - Leader brief display

--- src/app/leader/brief/leader.brief.component.ts
+++ src/app/leader/brief/leader.brief.component.ts
@@ -1,23 +1,51 @@
-import { Component, Output, OnInit, OnChange } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { LeaderModel, LeaderService } from '../../shared/leader/index';
 
 @Component({
   selector: 'app-leader-brief',
   templateUrl: './leader.brief.component.html',
   styleUrls: ['./leader.brief.component.css']
 })
-export class LeaderBriefComponent implements OnInit {
+export class LeaderBriefComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() leaderId = '';
+
+  leader: LeaderModel = new LeaderModel();
+
+  constructor(
+    private leaderService: LeaderService
+  ) {}
 
   ngOnInit() {
   }
 
-  ngOnChange(changes) {
+  ngOnChanges(changes) {
+    // console.log('chnages:', changes);
     if (changes.leaderId) {
       if (changes.leaderId.currentValue = 'random') {
-        console.log('get random leader');
+        console.log('Get random leader');
+        this.requestLeader(this.leaderId);
       }
     }
   }
 
+  requestLeader(id) {
+    this.leaderService.getLeader(id)
+    .subscribe(
+      data => {
+          this.setLeader(data);
+      },
+      err => console.error(err),
+      () => {}
+    );
+  }
+
+  /**
+   * Leader loading handler
+   * @param {data} Loaded leader data
+   */
+  setLeader(data) {
+    console.log('set leader: ', data);
+    this.leader = data;
+  }
 }
