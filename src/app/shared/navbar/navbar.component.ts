fd41e7955c5643194165f79a32c734348291e896
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-05-10 09:32:35 +0300

    Steps - Mobile Layout for XS screen width

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserService } from '../user';
-import { LeaderService } from '../leader';
-import { Subscription } from 'rxjs/Subscription';
 
 /**
  * This class represents the navigation bar component.
@@ -11,26 +8,4 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-
-  showCreateLeaderButton;
-  subscription: Subscription;
-
-  constructor(
-    public userService: UserService,
-    public leaderService: LeaderService
-  ) {}
-
-  ngOnInit() {
-    this.subscription = this.leaderService.leaderStream
-      .subscribe(item => {
-        // console.log('Navbar: showCreateLeaderButton =', this.showCreateLeaderButton);
-        this.showCreateLeaderButton = this.userService.authenticated() && !this.userService.hasLeader();
-      });
-  }
-
-  ngOnDestroy() {
-    // prevent memory leak when component is destroyed
-    this.subscription.unsubscribe();
-  }
-}
+export class NavbarComponent { }
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { UserService } from '../user/user.service';
-import { MdIconModule } from '@angular/material';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../user';
+import { LeaderService } from '../leader';
+import { Subscription } from 'rxjs/Subscription';
 
 /**
  * This class represents the toolbar component.
@@ -10,11 +11,34 @@ import { MdIconModule } from '@angular/material';
   templateUrl: 'toolbar.component.html',
   styleUrls: ['toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit, OnDestroy {
 
-  isDarkTheme = false;
+ // Show if user haven't got a Leader or anonymous
+ showCreateLeaderButton;
 
-  constructor(
-    public userService: UserService
-  ) {}
+ // Show if user have got a Leader
+ showCreateProjectButton;
+
+ subscription: Subscription;
+
+ constructor(
+   public userService: UserService,
+   public leaderService: LeaderService
+ ) {}
+
+ ngOnInit() {
+   this.subscription = this.leaderService.leaderStream
+     .subscribe(item => {
+       // FIXME - force update after User Login / Logout
+       console.log('Navbar: show Create Leader Button =', this.showCreateLeaderButton,
+       'showCreateProjectButton = ', this.showCreateProjectButton );
+       this.showCreateLeaderButton = !this.userService.hasLeader();
+       this.showCreateProjectButton = this.userService.hasLeader() && this.userService.authenticated();
+     });
+ }
+
+ ngOnDestroy() {
+   // prevent memory leak when component is destroyed
+   this.subscription.unsubscribe();
+ }
 }
