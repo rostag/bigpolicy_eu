aa999e4e2ca6beb92b4e2af408ee70c90efb972e
Rostyslav Siryk <rostislav.siryk@gmail.com>
2017-06-10 11:14:24 +0300

    Version increase to 1.1.42

--- utils/deploy-ci.sh
+++ utils/deploy-ci.sh
@@ -37,7 +37,7 @@ git add . && git commit -m 'deploy-update-build'
 ### 5. Push deploy branch to remote repo
 git push remote-ci deploy-ci --force
 
-### 6. Switch to temporary branch (safety - can be skipped)
+### 6. Switch to original branch (safety - can be skipped)
 git checkout $2 --force
 
 echo "----------B I G D E P L O Y-----------"
