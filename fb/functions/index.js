1af15e5b72867f692eb2e289f84519e69ba695d8
Rosty <rostyslav.siryk@gmail.com>
2017-11-29 20:46:40 +0200

    Firebase Express with CORS first route

--- fb/functions/index.js
+++ fb/functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const cors = require("cors");
+const express = require("express")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//     response.send("Hello from Firebase!");
+// });
+
+// exports.bigben = functions.https.onRequest((req, res) => {
+//     const hours = (new Date().getHours() % 12) + 1 // london is UTC + 1hr;
+//     res.status(200).send(`<!doctype html>
+//       <head>
+//         <title>Time</title>
+//       </head>
+//       <body>
+//         ${'BONG '.repeat(hours)}
+//       </body>
+//     </html>`);
+// });
+
+/* Express with CORS */
+const appExpress = express()
+appExpress.use(cors({ origin: true }))
+appExpress.get("*", (request, response) => {
+  response.send("Hello from Express on Firebase with CORS!")
+})
+
+const apiExpress = functions.https.onRequest(appExpress)
+
+module.exports = {
+  apiExpress
+}
