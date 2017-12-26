f15dac3ce46e0d31ecd514c37888dfad017f6c0e
Rosty <rostyslav.siryk@gmail.com>
2017-12-26 15:13:32 +0200

    FB Deployment Update

--- fb/functions/server/mongo/database.js
+++ fb/functions/server/mongo/database.js
@@ -11,13 +11,14 @@ var options = {
   }
 };
 
-// var MNG_URL = 'mongodb://bpqa:bpqa81@ds119436.mlab.com:19436/bpqa';
-var MNG_URL = 'mongodb://localhost:27017/bigpolicy';
+var prod = true;
+var MNG_URL = 'mongodb://bpqa:bpqa81@ds119436.mlab.com:19436/bpqa';
+// var MNG_URL = 'mongodb://localhost:27017/bigpolicy';
 
 try {
   console.log('  𝖄 Mongoose connection:');
-  if (MNG_URL) {
-    console.error('    Remote mongodb: ' + MNG_URL);
+  if (prod) {
+    console.log('    Remote mongodb: ' + MNG_URL);
     // FIXME_SEC
     // options.user = 'admin';
     // options.pass = app.qa;
@@ -25,10 +26,9 @@ try {
     mongoose.connect(MNG_URL, options);
   } else {
     // localhost
-    // options.useMongoClient = true;
-    console.error('    Local mongodb://localhost:27017/bigpolicy');
-    mongoose.connect(MNG_URL, options);
-    // mongoose.connect('mongodb://localhost:27017/bigpolicy', options);
+    options.useMongoClient = true;
+    console.log('    Local mongodb://localhost:27017/bigpolicy');
+    mongoose.connect('mongodb://localhost:27017/bigpolicy', options);
   }
 } catch (err) {
   console.error('A Mongoose connection failed with error: ', err);
