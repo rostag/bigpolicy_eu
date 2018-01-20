736ee010705816567260069e99786d550c8e17bd
Rosty <rostyslav.siryk@gmail.com>
2018-01-21 01:21:55 +0200

    Adopting local dev back to remote

--- fb/functions/index.js
+++ fb/functions/index.js
@@ -4,18 +4,7 @@ var middleware = require('./server/');
 var appExpress = express();
 var router = express.Router();
 
-function corsHeaders(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}
-
-// FIXME_SEC
-appExpress.use(corsHeaders);
-
 middleware(appExpress, router);
-
-appExpress.use('/api', router);
 
 exports.appExpress = functions.https.onRequest(appExpress);
 
