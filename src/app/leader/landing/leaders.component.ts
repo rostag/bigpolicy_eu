b9d9cabb14679865ede94e6ba7657757c21cbd2c
Rosty <rostyslav.siryk@gmail.com>
2018-01-29 01:59:15 +0200

    Leaders landing page added

---
+++ src/app/leader/landing/leaders.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaders',
+  templateUrl: './leaders.component.html',
+  styleUrls: ['./leaders.component.scss']
+})
+
+export class LeadersComponent { }
