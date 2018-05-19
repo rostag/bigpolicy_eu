b51bb4c3620e515ee849eb8cc02bad67da1e18f0
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-05-19 03:16:25 +0300

    Code cleanup

--- src/app/shared/dialog/dialog.service.ts
+++ src/app/shared/dialog/dialog.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs/Rx';
 import { DialogComponent } from './dialog.component';
 // FIXME Look can in be removed
 import { MatDialogRef, MatDialog, MatDialogConfig } from '@angular/material';
-import { ViewContainerRef, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DialogService {
@@ -13,15 +13,10 @@ export class DialogService {
     title: string,
     message: string,
     btnOkText: string = 'Продовжити',
-    btnCancelText: string = 'Відмінити',
-    viewContainerRef?: ViewContainerRef): Observable<boolean> {
-
-    // Docs: https://material.angular.io/components/component/dialog
-    // http://www.madhur.co.in/blog/2017/03/26/angular-confirmation-dialog.html
+    btnCancelText: string = 'Відмінити'): Observable<boolean> {
 
     let dialogRef: MatDialogRef<DialogComponent>;
     const dialogConfig = new MatDialogConfig();
-    // FIXME dialogConfig.viewContainerRef = viewContainerRef;
 
     dialogRef = this.dialog.open(DialogComponent, dialogConfig);
 
