20a87fda489ee8261f25d68bd05475b85a458f8d
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-06-09 15:38:13 +0300

    Pre-deploy - updated mongo url to remote, cleaned config up

--- fbs/functions/server/config.js
+++ fbs/functions/server/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   AUTH0_DOMAIN: 'bigpolicy.eu.auth0.com',
   AUTH0_API_AUDIENCE: 'http://bigpolicy.eu/api/',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27027/bigpolicy',
+  MONGO_URI: 'mongodb://bpqa:bpqa81@ds119436.mlab.com:19436/bpqa' || 'mongodb://localhost:27027/bigpolicy',
   NAMESPACE: 'https://bigpolicy.eu/roles'
 };
