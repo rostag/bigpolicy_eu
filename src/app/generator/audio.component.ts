ed0e1ad7723f99ac18c3023bf0ae77796fc9efea
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-06-22 01:40:47 +0300

    Added basic audio sampler

--- src/app/generator/audio.component.ts
+++ src/app/generator/audio.component.ts
@@ -1,23 +1,59 @@
 
 import { Component, OnInit } from '@angular/core';
 @Component({
-  selector: 'app-audio',
-  templateUrl: './audio.component.html',
-  styleUrls: ['./audio.component.scss']
+    selector: 'app-audio',
+    templateUrl: './audio.component.html',
+    styleUrls: ['./audio.component.scss']
 })
 export class AudioComponent implements OnInit {
 
-  public ac: AudioContext;
+    public ac: AudioContext;
 
-  constructor() { }
+    public loadingSample = false;
 
-  ngOnInit() {
-    this.createAudio();
-  }
+    private audioBuffer: AudioBuffer;
 
-  createAudio() {
-    this.ac = new AudioContext();
-    console.log('Init audio:');
-  }
+    constructor() { }
+
+    public ngOnInit() {
+        this.createAudio();
+
+        this.loadingSample = true;
+        this.fetchSample()
+            .then(audioBuffer => {
+                this.loadingSample = false;
+                this.audioBuffer = audioBuffer;
+            })
+            .catch(error => {
+                throw error;
+            });
+    }
+
+    public createAudio() {
+        this.ac = new AudioContext();
+        console.log(`Init audio:
+        https://ankursethi.in/2016/01/13/build-a-sampler-with-angular-2-webaudio-and-webmidi-lesson-1-introduction-to-the-webaudio-api/`);
+    }
+
+    public fetchSample(): Promise<AudioBuffer> {
+        return fetch('/assets/wav/kick.wav')
+            .then(response => response.arrayBuffer())
+            .then(buffer => {
+                return new Promise<AudioBuffer>((resolve, reject) => {
+                    this.ac.decodeAudioData(
+                        buffer,
+                        resolve,
+                        reject
+                    );
+                });
+            });
+    }
+
+    public playSample() {
+        const bufferSource = this.ac.createBufferSource();
+        bufferSource.buffer = this.audioBuffer;
+        bufferSource.connect(this.ac.destination);
+        bufferSource.start(0);
+    }
 
 }
