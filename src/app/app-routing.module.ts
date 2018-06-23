4dc87d3c4306a06be5296ecdbe562fa9bc7d5ada
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-06-23 19:46:54 +0300

    Refactored single core module to multiple material, bp and core modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoggedInGuard } from './shared/login/logged-in.guard';
-
-import { ProfileComponent } from '././shared/user/profile.component';
-import { AdminComponent } from '././shared/admin/admin.component';
-
-import { DisclaimerComponent } from './about/disclaimer/disclaimer.component';
-import { AboutComponent } from './about';
-import { HomeComponent } from './home';
-
-import { LeadersComponent } from './leader/landing';
-import { LeaderEditComponent } from './leader/edit';
-import { LeaderViewComponent } from './leader/view';
-
-import { ProjectsComponent } from './project/landing';
-import { ProjectEditComponent } from './project/edit';
-import { ProjectViewComponent } from './project/view';
-
-import { TaskEditComponent } from './task/edit';
-import { TaskListComponent } from './task/list';
-import { TaskViewComponent } from './task/view';
-
 // The order of routes is important. More specific come first.
 const routes: Routes = [
-  { path: 'leader/:id/edit', component: LeaderEditComponent },
-  { path: 'project/:id/edit', component: ProjectEditComponent },
-  { path: 'task/:id/edit', component: TaskEditComponent },
-  { path: 'leader/:id', component: LeaderViewComponent },
-  { path: 'project/:id', component: ProjectViewComponent },
-  { path: 'task/:id', component: TaskViewComponent },
-
-  { path: 'leaders', component: LeadersComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'tasks', component: TaskListComponent },
-  { path: 'add-leader', component: LeaderEditComponent },
-  { path: 'add-project', component: ProjectEditComponent },
-  { path: 'add-task', component: TaskEditComponent },
-
-  // The guard is added as an array, multiple guards will be executed in a sequence
-  // and only let the user see the page if all of them returns or resolves to true.
-  { path: 'profile', component: ProfileComponent, canActivate: [LoggedInGuard] },
-  { path: 'about', component: AboutComponent },
   { path: 'generator', loadChildren: 'app/generator/generator.module#GeneratorModule' },
-  { path: 'privacy', component: DisclaimerComponent },
-  { path: '', component: HomeComponent },
-
-  { path: 'admin', component: AdminComponent, canActivate: [LoggedInGuard] }
+  { path: '', loadChildren: 'app/bp.module#BpModule' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,79 @@
-import { AppComponent } from './app.component';
-import { CoreModule } from './core.module';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ToolbarComponent } from './shared';
+import { CoreModule } from './core.module';
+import { MaterialModule } from './common/modules/material/material.module';
+import { WorkingSpinnerComponent } from './shared/xhr/spinner.component';
+import { EffectsModule } from '@ngrx/effects';
+import { LeaderEffects } from './state/effects/leader.effects';
+import { ProjectEffects } from './state/effects/project.effects';
+import { TaskEffects } from './state/effects/task.effects';
+import { StoreModule } from '@ngrx/store';
+import { reducer as authReducer } from './state/reducers/auth.reducers';
+import { reducer as leadersReducer } from './state/reducers/leader.reducers';
+import { reducer as projectsReducer } from './state/reducers/project.reducers';
+import { reducer as tasksReducer } from './state/reducers/task.reducers';
+// import { counterReducer } from './state/reducers/counter.reducers';
+// import { AuthEffects } from './state/effects/auth.effects';
+import { LeaderService } from './shared/leader';
+import { ProjectService } from './shared/project';
+import { TaskService } from './shared/task';
+import { DialogService } from './shared/dialog/dialog.service';
+import { UserService } from './shared/user/user.service';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogComponent } from './shared/dialog/dialog.component';
+
+// FIXME Re-enable after modules refactoring
+// import { CustomBrowserXhr } from './shared/xhr/xhr';
+// import { XhrFactory } from '@angular/common/http';
 
 @NgModule({
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CoreModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    EffectsModule.forRoot([
+      LeaderEffects,
+      ProjectEffects,
+      TaskEffects,
+      // AuthEffects
+    ]),
+    StoreModule.forRoot({
+      authState: authReducer,
+      leadersState: leadersReducer,
+      projectsState: projectsReducer,
+      tasksState: tasksReducer
+    })
+  ],
+  exports: [
+    MaterialModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    WorkingSpinnerComponent,
+    DialogComponent
+  ],
+  providers: [
+    LeaderService,
+    ProjectService,
+    TaskService,
+    DialogService,
+    UserService,
+    Title
+    // FIXME Re-enable after modules refactoring
+    // CustomBrowserXhr,
+    // { provide: XhrFactory, useExisting: CustomBrowserXhr }
   ],
   bootstrap: [
     AppComponent
+  ],
+  entryComponents: [
+      DialogComponent
   ]
 })
-export class AppModule {}
+export class AppModule { }
