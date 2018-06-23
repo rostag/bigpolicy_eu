e9e9613fccb32f1c0d31ad2f33516839fee0a914
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-06-23 15:13:33 +0300

    Updated main nav implementation to use links not buttons, cleaned code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { AppComponent } from './app.component';
 import { CoreModule } from './core.module';
 import { NgModule } from '@angular/core';
-import { routing } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     CoreModule,
-    routing
+    AppRoutingModule
   ],
   declarations: [
     AppComponent
