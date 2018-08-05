ec088d99b8ea0fc081b47cb6b5b97cb73b881556
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-08-05 16:51:09 +0300

    Add Region field to location component (hard-coded values)

--- src/app/common/location/location.component.ts
+++ src/app/common/location/location.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationService } from './location.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-location',
@@ -10,13 +13,29 @@ export class LocationComponent implements OnInit {
 
   private locationOptions = null;
 
+  public myControl = new FormControl();
+  public options: string[] = ['Київ', 'Харківська', 'Львівська'];
+  public filteredOptions: Observable<string[]>;
+
   constructor(private locationService: LocationService) { }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.locationService.getRegions().subscribe(result => {
       this.locationOptions = result;
       console.log('>> location options:', this.locationOptions);
     });
     // this.locationService.getCitiesForRegion(1);
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
