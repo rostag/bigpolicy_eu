7097d410c978f9e3eeb46cfd459f1e52c93e769a
Rosty <Rostyslav.Siryk@externals.hilti.com>
2019-02-19 00:32:13 +0200

    Clean code up

--- src/app/shared/video/video.component.ts
+++ src/app/shared/video/video.component.ts
@@ -9,9 +9,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 export class VideoComponent {
 
-  private thumbUrl;
   public youTubeId;
+
   public safeMediaUrl;
+
+  private thumbUrl;
 
   @Input() title = '';
 
@@ -22,14 +24,12 @@ export class VideoComponent {
       const match = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
       this.youTubeId = (match && match[7].length === 11) ? match[7] : null;
 
-      // FIXME_SEC Set safe media URL
       this.safeMediaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
         this.youTubeId
           ? `https://www.youtube.com/embed/${this.youTubeId}`
           : null
       );
 
-      // Set thumb URL by video
       this.thumbUrl = this.youTubeId
         ? `http://img.youtube.com/vi/${this.youTubeId}/0.jpg`
         : `assets/img/project/project-placeholder.png`;
