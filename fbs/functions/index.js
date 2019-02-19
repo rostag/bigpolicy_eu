a607f84e694fdd28a9f6c208e49bb41fd92d2d91
Rosty <Rostyslav.Siryk@externals.hilti.com>
2019-02-20 00:07:24 +0200

    Code cleanup

--- fbs/functions/index.js
+++ fbs/functions/index.js
@@ -1,11 +1,6 @@
-// bp/fbs/functions/index.js
-
-var express = require('express');
-var functions = require('firebase-functions');
-var middleware = require('./server/');
-var appExpress = express();
-var router = express.Router();
-
-middleware(appExpress, router);
-
-exports.appExpress = functions.https.onRequest(appExpress);
+const express = require('express');
+require('./server/')(express(), express.Router());
+exports
+  .appExpress = require('firebase-functions')
+  .https
+  .onRequest(appExpress);
