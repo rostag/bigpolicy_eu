bbba9e595e1526c55a9081c95af33f2e1c013580
Rosty <Rostyslav.Siryk@externals.hilti.com>
2019-02-19 02:27:19 +0200

    Move konfig to the root

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-import * as K from '../../fbs/functions/konfig';
+import * as K from '../../konfig';
 
 export const environment = {
   production: false,
