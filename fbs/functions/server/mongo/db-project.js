95153481f5b77376a38782b116aadf783eedf554
Rosty <rostislav.siryk@gmail.com>
2019-04-27 15:06:57 +0300

    Fix project creation

--- fbs/functions/server/mongo/db-project.js
+++ fbs/functions/server/mongo/db-project.js
@@ -2,74 +2,71 @@
 // P R O J E C T
 //******************************************************************************
 
-var DBProject = {};
-var Project = require('./models/project');
-var Leader = require('./models/leader');
-var mongoose = require('mongoose');
+let DBProject = {};
+let Project = require('./models/project');
+let Leader = require('./models/leader');
+let mongoose = require('mongoose');
 const DBTask = require('./db-task');
-const DBLeader = require('./db-leader');
 
 /**
  * Creates a Project by provided data
  * @param dataObj Properties object of Project to be created
  */
-DBProject.createProject = function(dataObj) {
-  var data = dataObj;
-  for ( var item in dataObj ) {
+DBProject.createProject = async function (dataObj) {
+  let data = dataObj;
+  for (let item in dataObj) {
     data = JSON.parse(item);
   }
 
   if (!data.title || !data.description) {
-    throw ('DBProject: Invalid project cannot be saved. Either title or description is missed.')
+    console.log('Error saving project data:', data);
+    throw (`DBProject: Invalid project cannot be saved. Either title or description is missed. Received dataObj: ${JSON.stringify(dataObj, null, '  ')}`)
   }
 
   if (!data) data = {};
 
-  const projectModel = new Project(data);
-  var addedToLeader = projectModel.save( (err, project) => {
-    if (err) { console.log('Project saving error: ', err); }
-    DBProject.addOrRemoveProjectForLeader([project._id], project.managerId, false);
-  });
-  return projectModel.save(addedToLeader);
+  const projectModel = await new Project(data).save();
+  DBProject.addOrRemoveProjectForLeader([projectModel._id], projectModel.managerId, false);
+  return projectModel;
 }
 
 /**
  * Returns single Project by ID
  */
-DBProject.getProject = function(projectId) {
+DBProject.getProject = function (projectId) {
   //  console.log('DB :: getProject, id:', projectId);
 
-   if (projectId === 'random') {
-     return Project.countDocuments().exec()
-       .then((cnt, err) => {
-         const rndm = Math.floor(Math.random() * cnt);
-         return Project.findOne().skip(rndm).exec()
-           .then((randomLeader, err) => {
-             if(randomLeader){
-               console.log(`Random ${rndm} of ${cnt} = ${randomLeader._id}`);
-             }
-             return randomLeader;
-           })
-       });
-   } else {
-     return Project.findById(projectId);
-   }
+  if (projectId === 'random') {
+    return Project.countDocuments().exec()
+      .then((cnt) => {
+        const rndm = Math.floor(Math.random() * cnt);
+        return Project.findOne().skip(rndm).exec()
+          .then((randomLeader) => {
+            if (randomLeader) {
+              console.log(`Random ${rndm} of ${cnt} = ${randomLeader._id}`);
+            }
+            return randomLeader;
+          })
+      });
+  } else {
+    return Project.findById(projectId);
+  }
 }
 
 /**
-  * Returns a page of Projects either by given projectIds (if present), page number and limit, or DB query
-  * @param projectIds {Array} project ID's to retrieve
-  * @param page {number} Page number to get from DB
-  * @param limit {number} Items per page to get from DB
-  * @param dbQuery {string} DB query to perform for filtering the results, searching etc
-  * Usage in HTML code:
-  <app-project-list
-    title="Проекти з заходами"
-    [leaderId]="profileLeader._id"
-    pageSize="3"
-    dbQuery='{ "$where": "this.taskIds.length > 0" }'>
-  </app-project-list>
-**/
+ * Returns a page of Projects either by given projectIds (if present), page number and limit, or DB query
+ * @param projectIds {Array} project ID's to retrieve
+ * @param page {number} Page number to get from DB
+ * @param limit {number} Items per page to get from DB
+ * @param dbQuery {string} DB query to perform for filtering the results, searching etc
+ * Usage in HTML code:
+ <app-project-list
+ title="Проекти з заходами"
+ [leaderId]="profileLeader._id"
+ pageSize="3"
+ dbQuery='{ "$where": "this.taskIds.length > 0" }'>
+ </app-project-list>
+ **/
 DBProject.getPageOfProjects = function (projectIds, page, limit, dbQuery) {
 
   var query = {};
@@ -81,24 +78,24 @@ DBProject.getPageOfProjects = function (projectIds, page, limit, dbQuery) {
 
   // If passed, use project IDs in query
   if (projectIds) {
-    query['_id'] = { $in: projectIds };
+    query['_id'] = {$in: projectIds};
   }
 
-  return Project.paginate(query, { page: parseInt(page), limit: parseInt(limit) });
+  return Project.paginate(query, {page: parseInt(page), limit: parseInt(limit)});
 }
 
-DBProject.updateProject = function(id,data) {
+DBProject.updateProject = function (id, data) {
 
-  if ( !data.title || !data.description ) {
-    throw ( 'DBProject: Invalid project cannot be updated. Either title or description is missed.')
+  if (!data.title || !data.description) {
+    throw ('DBProject: Invalid project cannot be updated. Either title or description is missed.')
   }
 
-  if(!data) {
+  if (!data) {
     data = {};
   }
 
-  return Project.findById(id, function(err, model) {
-    if(err || !model){
+  return Project.findById(id, function (err, model) {
+    if (err || !model) {
       return;
     }
     for (var field in data) {
@@ -114,20 +111,20 @@ DBProject.updateProject = function(id,data) {
  * @param data Data to set in format { managerId: value }
  * @private
  */
- // TODO Check it's better to refactor to make single method of update, merging this method with updateProject (above)
- // http://codingmiles.com/nodejs-bulk-update-to-mongodb-using-mongoose/
- // https://www.mongodb.com/blog/post/mongodbs-new-bulk-api
- // http://stackoverflow.com/questions/28218460/nodejs-mongoose-bulk-update
-DBProject.bulkUpdateProjects = function(projectIds, data) {
+// TODO Check it's better to refactor to make single method of update, merging this method with updateProject (above)
+// http://codingmiles.com/nodejs-bulk-update-to-mongodb-using-mongoose/
+// https://www.mongodb.com/blog/post/mongodbs-new-bulk-api
+// http://stackoverflow.com/questions/28218460/nodejs-mongoose-bulk-update
+DBProject.bulkUpdateProjects = function (projectIds, data) {
   console.log('DBProject.bulk UpdateProjects:', projectIds, data);
   var bulk = Project.collection.initializeOrderedBulkOp();
   for (var i = 0; i < projectIds.length; i++) {
-      var id = projectIds[i];
-      bulk.find({
-          '_id': mongoose.Types.ObjectId(id)
-      }).updateOne({
-          $set: data
-      });
+    var id = projectIds[i];
+    bulk.find({
+      '_id': mongoose.Types.ObjectId(id)
+    }).updateOne({
+      $set: data
+    });
   }
   DBProject.addOrRemoveProjectForLeader(projectIds, data.managerId, false);
 
@@ -140,11 +137,11 @@ DBProject.bulkUpdateProjects = function(projectIds, data) {
  * @param projectIds: {Array} Project IDs to adding or remove.
  * @param managerId {string} Target Leader ID.
  */
-DBProject.addOrRemoveProjectForLeader = function(projectIds, managerId, toRemove) {
+DBProject.addOrRemoveProjectForLeader = function (projectIds, managerId, toRemove) {
   console.log('> DBProject.add Or Remove Project For Leader, managerId:', managerId);
 
-  Leader.findOne( { _id: managerId }, function (err, leader) {
-    if( leader ) {
+  Leader.findOne({_id: managerId}, function (err, leader) {
+    if (leader) {
       console.log(`\tLeader found: ${leader.name}\n\tHis projectIds: ', ${leader.projectIds}\n\tA projectIds to`, toRemove ? 'remove:' : 'add:', projectIds);
 
       for (var i = 0; i < projectIds.length; i++) {
@@ -152,25 +149,27 @@ DBProject.addOrRemoveProjectForLeader = function(projectIds, managerId, toRemove
         const projectIndex = leader.projectIds.indexOf(projectId);
         if (toRemove === true) {
           leader.projectIds.splice(projectIndex, 1);
-        } else if ( projectIndex === -1 ) {
+        } else if (projectIndex === -1) {
           leader.projectIds.push(projectId);
         }
       }
       console.log('\tNow projectIds: ', leader.projectIds);
 
-      leader.update({ projectIds: leader.projectIds }, (error, leader) => { console.log('< Leader projectIds updated'); });
+      leader.update({projectIds: leader.projectIds}, (error, leader) => {
+        console.log('< Leader projectIds updated');
+      });
     }
   });
 }
 
-DBProject.deleteProject = function(id) {
+DBProject.deleteProject = function (id) {
   return Project.findById(id).then(project => {
     DBProject.addOrRemoveProjectForLeader([project._id], project.managerId, true);
     return project.remove();
   });
 }
 
-DBProject.bulkDeleteProjects = function(projectIds) {
+DBProject.bulkDeleteProjects = function (projectIds) {
   var bulk = Project.collection.initializeOrderedBulkOp();
   console.log('> DBProject.bulkDeleteProjects:', projectIds.length);
 
@@ -199,7 +198,7 @@ DBProject.bulkDeleteProjects = function(projectIds) {
         .then(tasksDeleted => {
           console.log('< Tasks deleted:', tasksDeleted);
         })
-        .catch(function(err){
+        .catch(function (err) {
           console.log('< Error of task deletion:', tasksDeleted);
         });
     }
