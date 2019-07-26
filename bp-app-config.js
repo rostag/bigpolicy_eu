be61c25de2a83c19f06b88f9f87c2e09db8af83c
rostyslav-siryk-gl <rostyslav.siryk@globallogic.com>
2019-07-26 21:59:19 +0300

    Updating config

--- bp-app-config.js
+++ bp-app-config.js
@@ -1,25 +1,24 @@
 // bp-app-config.js
-// Update config
 
 module.exports = {
-    liq: {
-      donationPrefix: '',
-      dataSeparator: '',
-      public_key: 'xxxxxxxxxxxx',
-      private_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    },
-    mailgun: {
-      apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-      domain: 'bigpolicy.eu'
-    },
-    gdrive: {
-      client_id: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    },
-    fbs: {
-      apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-      authDomain: 'testbase-xxxxx.firebaseapp.com',
-      databaseURL: 'https://testbase-xxxxx.firebaseio.com',
-      storageBucket: 'testbase-xxxxx.appspot.com',
-      messagingSenderId: 'xxxxxxxxxxxx'
-    }
-  };
+  liq: {
+    donationPrefix: 'bpdon___id_',
+    dataSeparator: '-BGPLCXX-',
+    public_key: 'i77061351482',
+    private_key: 'v26xvBR6w6h9JVxGKLCSDzHnHg6GpIEoQeECKhuG'
+  },
+  mailgun: {
+    apiKey: 'key-fbb7eae260ccda81270645824316856a',
+    domain: 'bigpolicy.eu'
+  },
+  gdrive: {
+    client_id: '254701279966-lgp72d0ou71o9865v7tp55fmc08ac661.apps.googleusercontent.com'
+  },
+  fbs: {
+    apiKey: 'AIzaSyCa_yL-SOkz0-x-cdzuRJRTmbzs-5VNNp0',
+    authDomain: 'testbase-eb57f.firebaseapp.com',
+    databaseURL: 'https://testbase-eb57f.firebaseio.com',
+    storageBucket: 'testbase-eb57f.appspot.com',
+    messagingSenderId: '780191546457'
+  }
+};
