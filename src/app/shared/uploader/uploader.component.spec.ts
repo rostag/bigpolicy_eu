8266d9b5fa72bc776301d3777fd128e706d45a1c
rostyslav-siryk-gl <rostyslav.siryk@globallogic.com>
2019-07-29 11:17:43 +0300

    Cleaning up

--- src/app/shared/uploader/uploader.component.spec.ts
+++ src/app/shared/uploader/uploader.component.spec.ts
@@ -17,10 +17,9 @@ xdescribe('UploaderComponent', () => {
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule,
         // FIXME FIREBASE_CONFIG
-//       const firebase = require('firebase');
-//     const admin = require('firebase-admin');
-// // Initialize Firebase
-//     firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
+        //    const firebase = require('firebase');
+        //    const admin = require('firebase-admin');
+        //    firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
         AngularFireModule.initializeApp(firebaseConfig)
       ],
       providers: [ ProjectService, TaskService, DialogService, AngularFireDatabase ],
