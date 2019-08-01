fb003cf02d40816e7a6e83f8d7c931b052e3af40
Rosty <rostyslav.siryk@gmail.com>
2019-08-02 01:20:42 +0300

    Fixing runtime initialization errors

--- src/app/project/edit/project.edit.component.ts
+++ src/app/project/edit/project.edit.component.ts
@@ -50,6 +50,7 @@ export class ProjectEditComponent implements OnInit {
    */
   ngOnInit() {
     this.route.params.subscribe(params => {
+      console.log('Project Edit init:', params);
       if (params.id) {
         this.isUpdateMode = true;
         this.projectStore.dispatch(new LoadProject(params.id));
