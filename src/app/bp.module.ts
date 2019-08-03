7bdfc7791da065cdc278fb4d0cd6ca7f940871a4
rostyslav-siryk-gl <rostyslav.siryk@globallogic.com>
2019-08-03 21:34:45 +0300

    Tweak FBS DB in uploader-2 Moving FBS config

--- src/app/bp.module.ts
+++ src/app/bp.module.ts
@@ -57,7 +57,16 @@ import {CompanyComponent} from './companies/brief/company/company.component';
 import {QuestionsFormComponent} from './shared/questions-form/questions-form.component';
 
 export const K = environment.K;
-export const firebaseConfig = K.fbs;
+// export const firebaseConfig = K.fbs;
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCa_yL-SOkz0-x-cdzuRJRTmbzs-5VNNp0',
+  authDomain: 'testbase-eb57f.firebaseapp.com',
+  databaseURL: 'https://testbase-eb57f.firebaseio.com',
+  projectId: 'bigpolicy-qa',
+  storageBucket: 'testbase-eb57f.appspot.com',
+  messagingSenderId: '780191546457'
+};
 
 @NgModule({
   imports: [
@@ -67,14 +76,13 @@ export const firebaseConfig = K.fbs;
     BpRoutingModule,
     CookieLawModule,
     Ng2PaginationModule,
-    AngularFireDatabaseModule,
     // FIXME FIREBASE_CONFIG
     //    const firebase = require('firebase');
     //    const admin = require('firebase-admin');
     //    firebase.initialize App(JSON.parse(process.env.FIREBASE_CONFIG));
     // AngularFireModule.initialize App(firebaseConfig),
-    // FIXME
     AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
     NguCarouselModule
   ],
   exports: [
