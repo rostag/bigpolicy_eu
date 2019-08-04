356ce4c1e9a37ce4e158f1fe37e363bad9adbbda
Rostag <rostislav.siryk+github@gmail.com>
2019-08-05 00:31:59 +0300

    Fixing upload errors

--- src/app/shared/uploader/uploader.component.ts
+++ src/app/shared/uploader/uploader.component.ts
@@ -132,23 +132,50 @@ export class UploaderComponent implements OnChanges {
       console.log(`Upload a file, path = ${path}, selectedFile: ${selectedFile}, folder = ${folder}, iRef = ${iRef}`);
       const uploadTask = iRef.put(selectedFile);
 
-      // uploadTask.then((snapshot) => {
-      //   console.log('Upload snapshot:', snapshot, path);
-      //   this.uploadedFileUrl = snapshot.downloadURL;
-      //   this.afDb.list(`/${folder}`).push({path: path, filename: selectedFile.name});
-      //   this.onFileUploadComplete();
-      // });
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error) => {
+          console.log('ERRORED:', error)
 
-      uploadTask.snapshot.ref.getDownloadURL()
-        .then(
-          (downloadURL) => {
-            console.log('File available at: ', downloadURL);
-            this.uploadedFileUrl = downloadURL;
-            this.afDb.list(`/${folder}`).push({path: path, filename: selectedFile.name});
-            this.onFileUploadComplete();
-          },
-          (error) => console.error('Error uploading file', JSON.stringify(error, null, '  '))
-        );
+          // A full list of error codes is available at https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error['code']) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              break;
+
+            case 'storage/canceled':
+              // User canceled the upload
+              break;
+
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        }, () => {
+          uploadTask.snapshot.ref.getDownloadURL()
+            .then(
+              (downloadURL) => {
+                console.log('Uploaded file available at: ', downloadURL);
+                this.uploadedFileUrl = downloadURL;
+                this.afDb.list(`/${folder}`).push({path: path, filename: selectedFile.name});
+                this.onFileUploadComplete();
+              },
+              (error) => console.error('Error uploading file: ', error)
+            );
+        });
     }
   }
 
